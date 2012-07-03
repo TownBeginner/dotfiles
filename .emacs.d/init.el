6f658225b0e2ba3c2176b4b6387b7980c7321f70
TownBeginner <townbeginner6290+git@gmail.com>
2012-07-03 22:16:19 +0900

    First commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,15 @@
+;; loa-pathを追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((defaultly-directory
+               (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; 引数のサブディレクトリとそのサブディレクトリをload-pathに追加
+(add-to-load-path "elisp" "conf" "public_repos")
+
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/conf") ; 設定ファイルがあるディレクトリを指定
