8ebb0bb3f9140de4fd0b3a65495dcc4691f74f13
TownBeginner <townbeginner6290+git@gmail.com>
2012-07-08 20:49:56 +0900

    add ruby-mode and others

---
+++ .emacs.d/undohist/!Users!onozakishinichi!.emacs.d!elisp!inf-ruby.el
@@ -0,0 +1,2 @@
+
+((digest . "4dd70a74b42ad89a50b9ac7012db5827") (undo-list nil (1 . 15128) (t . 0)))
---
+++ .emacs.d/undohist/!Users!onozakishinichi!.emacs.d!elisp!ruby-electric.el
@@ -0,0 +1,2 @@
+
+((digest . "4b6698380d24e433dca172922fa49ff4") (undo-list nil (1 . 7316) (t . 0)))
