ac4b1a98fcecc15b1334ad39a5ae1659c372dac1
TownBeginner <townbeginner6290+git@gmail.com>
2012-07-08 23:52:05 +0900

    modify add-to-load-path

---
+++ .emacs.d/backups/#!Users!onozakishinichi!Documents!RubyPractice!advanced!mymethod1.rb#
@@ -0,0 +1,17 @@
+class A
+  def foo
+    "FOO"
+  end
+
+  def bar
+    foo = "XXX"
+    puts foo + self.foo
+  end
+  def foobar
+    puts foo
+  end
+end
+
+a = A.new
+a.bar
+a.foobar
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,7 @@
 (defun add-to-load-path (&rest paths)
   (let (path)
     (dolist (path paths paths)
-      (let ((defaultly-directory
+      (let ((default-directory
                (expand-file-name (concat user-emacs-directory path))))
         (add-to-list 'load-path default-directory)
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
